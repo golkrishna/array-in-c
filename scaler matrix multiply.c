661143dd11f95607ab9f9da696fa060cd1c286d7
golkrishna <121308011+golkrishna@users.noreply.github.com>
2023-05-09 11:47:01 +0530

    Create scaler matrix multiply.c

---
+++ scaler matrix multiply.c
@@ -0,0 +1,17 @@
+
+#include <stdio.h>
+
+int main() {
+   int arr[4];
+   
+   for(int i=0; i<4; i++){
+       printf("Enter the value of %d element in the array\n",i);
+       scanf("%d",&arr[i]);
+   }
+   for(int i=0; i<4; i++)
+           printf("the value of %d element of the array is %d\n",i,arr[i]);
+   }
+    
+   
+    return 0;
+}
